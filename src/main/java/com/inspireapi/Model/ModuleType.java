8dbe3eab27f013415f2eb21721825ced60f8bdfd
Gwewe <wedna.guirand@gmail.com>
2025-08-28 21:10:44 +0100

    Updated the moduleType enum with description name for user

--- src/main/java/com/inspireapi/Model/ModuleType.java
+++ src/main/java/com/inspireapi/Model/ModuleType.java
@@ -1,7 +1,17 @@
 package com.inspireapi.Model;
 
 public enum ModuleType {
-    BREATHE,
-    LEARN,
-    QUOTES
+    BREATHE("This is your time to stop and just breathe."),
+    LEARN("Expand your mind, one fact at a time."),
+    QUOTES("We work so hard, not to change the world but to prevent the world from changing us. - Unknown author");
+
+    private final String moduleDescription;
+
+    ModuleType(String moduleDescription) {
+        this.moduleDescription = moduleDescription;
+    }
+
+    public String getModuleDescription() {
+        return moduleDescription;
+    }
 }
