8a6d42aea36302d57ec6532558c63e5bad99637f
Gwewe <wedna.guirand@gmail.com>
2025-09-02 19:43:50 +0100

    Added the CRUD method to the service interface

--- src/main/java/com/inspireapi/Service/InspireSessionService.java
+++ src/main/java/com/inspireapi/Service/InspireSessionService.java
@@ -1,16 +1,48 @@
 package com.inspireapi.Service;
 
-public class InspireSessionService {
-    
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-    public void addModule(String breatheModuleOne) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addModule'");
-    }
+import com.inspireapi.Model.InspireSession;
 
-    public Object getModules() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getModules'");
-    }
+public interface InspireSessionService {
 
+
+    /**
+     * Get all InspireSessions.
+     * @return a list of all InspireSessions
+     */
+    List<InspireSession> getAllSessions();
+
+    /**
+     * Get an InspireSession by its ID.
+     * @param sessionId the ID of the InspireSession
+     * @return the InspireSession with the specified ID, or null if not found
+     */
+    Optional<InspireSession> getSessionById(UUID sessionId);
+
+
+    /**
+     * Create a new InspireSession.
+     * @param inspireSession the InspireSession to create
+     * @return the created InspireSession
+     */
+    InspireSession createInspireSession(InspireSession inspireSession);
+
+    /**
+     * Update all modules for a single session.
+     * @param sessionId the ID of the InspireSession to update
+     * @param inspireSession the updated InspireSession data
+     * @return the updated InspireSession, or null if not found
+     */
+    Optional<InspireSession> updateInspireSession(UUID sessionId, InspireSession inspireSession);
+
+
+    /**
+     * Delete an InspireSession by its ID.
+     * @param sessionId the ID of the InspireSession to delete
+     * @return true if the InspireSession was deleted, false if not found
+     */
+    void deleteInspireSession(UUID sessionId); 
 }
---
+++ src/main/java/com/inspireapi/Service/InspireSessionServiceImpl.java
@@ -0,0 +1,16 @@
+package com.inspireapi.Service;
+
+public class InspireSessionServiceImpl implements InspireSessionService {
+
+    
+    public void addModule(String breatheModuleOne) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addModule'");
+    }
+
+    public Object getModules() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getModules'");
+    }
+
+}
