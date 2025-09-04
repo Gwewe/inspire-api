b6d798c8edd2d78d1a4fd4f0dbf33fab2e35eefa
Gwewe <wedna.guirand@gmail.com>
2025-09-04 23:09:16 +0100

    Refactor the modules tables and refactor the module entity

--- src/main/java/com/inspireapi/Model/Module.java
+++ src/main/java/com/inspireapi/Model/Module.java
@@ -1,22 +1,41 @@
 package com.inspireapi.Model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "modules")
 public class Module {
+
+    @Id
+    @Column(name = "module_id")
+    private UUID moduleId;
     
     @Enumerated(EnumType.STRING)
+    @Column(name = "module_type")
     private ModuleType moduleType;
+    
+    @Column(name = "module_content")
     private String moduleContent;
 
-    public Module(ModuleType moduleType, String moduleContent) {
+    public Module(UUID moduleId, ModuleType moduleType, String moduleContent) {
+        this.moduleId = moduleId;
         this.moduleType = moduleType;
         this.moduleContent = moduleContent;
     }
 
     // Getters
+
+    public UUID getModuleId() {
+        return moduleId;
+    }
+
     public ModuleType getModuleType() {
         return moduleType;
     }
