7a0e92be1039d17cdc7bd748fbd8bfd425bbbc6e
Gwewe <wedna.guirand@gmail.com>
2025-09-05 20:38:15 +0100

    remove query in the module repository since spring boot data jpa help reduce boilerplate code

--- src/main/java/com/inspireapi/Repository/ModuleRepository.java
+++ src/main/java/com/inspireapi/Repository/ModuleRepository.java
@@ -4,12 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ModuleRepository extends JpaRepository<Module, UUID> {
 
     /**
-     * Find all modules of a specific type.
+     * Find all modules of a specific type
      * @param moduleType the type of the module
      * @return a list of modules matching the specified type
      */
@@ -17,11 +16,10 @@ public interface ModuleRepository extends JpaRepository<Module, UUID> {
 
 
     /**
-     * Find all modules containing a specific keyword in the module content, ignoring case.
-     * @param keyword the keyword to search for
-     * @return a list of modules containing the specified keyword
+     * Find modules containing a specific keyword in the module content
+     * @param keyword the keyword to search for in the module content
+     * @return a list of modules containint the keyword
      */
-    @Query("SELECT m FROM Module m WHERE LOWER(m.module_content) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    List<Module> searchModuleContentByKeywordIgnoreCase(String keyword);
+    List<Module> findByModuleContentContainingIgnoreCase(String keyword);
 
 }
