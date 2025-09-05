9b7a10e4166472d20f0faf0cdf62ca75112f03c9
Gwewe <wedna.guirand@gmail.com>
2025-09-05 16:12:04 +0100

    Refactor the moduletype enum

--- src/main/java/com/inspireapi/Model/ModuleType.java
+++ src/main/java/com/inspireapi/Model/ModuleType.java
@@ -1,10 +1,9 @@
 package com.inspireapi.Model;
-
 public enum ModuleType {
 
     BREATHE("This is your time to stop and just breathe."),
     LEARN("Expand your mind, one fact at a time."),
-    QUOTE("This is your time to reflect on a meaningful quote."),;
+    QUOTE("This is your time to reflect on a meaningful quote.");
 
     private final String moduleDescription;
 
