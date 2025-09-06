85bd9456cfe7e01e0c3cded9f1122845a70ad36e
Gwewe <wedna.guirand@gmail.com>
2025-09-06 14:29:13 +0100

    Added the module entity to the module repository as Java lang also had a object called module which was creating error in the service implementation

--- src/main/java/com/inspireapi/Repository/ModuleRepository.java
+++ src/main/java/com/inspireapi/Repository/ModuleRepository.java
@@ -1,11 +1,15 @@
 package com.inspireapi.Repository;
 
+
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.inspireapi.Model.Module;
 import com.inspireapi.Model.ModuleType;
+
+
 
 public interface ModuleRepository extends JpaRepository<Module, UUID> {
 
