1d09cc35444a9a9acae01d6f18e4a8666d58ea74
Gwewe <wedna.guirand@gmail.com>
2025-09-06 23:13:35 +0100

    Added the getsessionbyid and create inspire session to the controller

--- src/main/java/com/inspireapi/Controller/InspireSessionController.java
+++ src/main/java/com/inspireapi/Controller/InspireSessionController.java
@@ -1,9 +1,14 @@
 package com.inspireapi.Controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inspireapi.Model.InspireSession;
@@ -24,8 +29,26 @@ public class InspireSessionController {
     public ResponseEntity<List<InspireSession>> getAllSessions() {
         return ResponseEntity.ok(inspireSessionService.getAllSessions());
     }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<InspireSession> getSessionById(@PathVariable UUID sessionId) {
+        return ResponseEntity.ok(inspireSessionService.getSessionById(sessionId));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<InspireSession> createInspireSession(@RequestBody InspireSession inspireSession) {
+        InspireSession newInspireSessionCreated = inspireSessionService.createInspireSession(inspireSession);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newInspireSessionCreated);
+    }
+
+
+    // @PostMapping("/from-modules")
+    // public ResponseEntity<InspireSession> createInspireSessionFromModules(@RequestBody Map<ModuleType, UUID> moduleTypeToIdMap) {
+    //     InspireSession newInspireSessionFromModule = inspireSessionService.createInspireSessionFromModules(moduleTypeToIdMap);
+
+  
+    // }
     
-
-
 
 }
