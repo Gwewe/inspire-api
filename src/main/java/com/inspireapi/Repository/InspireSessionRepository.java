adfc4e119bc7d7ff2847c6275113a1db2c940de2
Gwewe <wedna.guirand@gmail.com>
2025-09-06 17:26:20 +0100

    Updated the comment and completed the findByLearnContentContainingIgnoreCase on the service impl

--- src/main/java/com/inspireapi/Repository/InspireSessionRepository.java
+++ src/main/java/com/inspireapi/Repository/InspireSessionRepository.java
@@ -14,21 +14,21 @@ import com.inspireapi.Model.InspireSession;
 public interface InspireSessionRepository extends JpaRepository<InspireSession, UUID> {
 
     /**
-     * Search for Inspire sessions containing a specific keyword in the breatheContent
+     * Find Inspire sessions containing a specific keyword in the breatheContent
      * @param breatheKeyword the keyword to search for in breatheContent
      * @return a list of InspireSessions containing the keyword in breatheContent
      */
     List<InspireSession> findByBreatheContentContainingIgnoreCase(String breatheKeyword);
 
     /**
-     * Search for Inspire sessions containing a specific keyword in the learnContent
+     * Find Inspire sessions containing a specific keyword in the learnContent
      * @param learnKeyword the keyword to search for in learnContent
      * @return a list of InspireSessions containing the keyword in learnContent
      */
     List<InspireSession> findByLearnContentContainingIgnoreCase(String learnKeyword);
 
     /**
-     * Search for Inspire sessions containing a specific keyword in the quoteContent
+     * Find Inspire sessions containing a specific keyword in the quoteContent
      * @param quoteKeyword the keyword to search for in quoteContent
      * @return a list of InspireSessions containing the keyword in quoteContent
      */
